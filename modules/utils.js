4cfed9af2da5f22f6178da9339f19ca9b20795b9
Jaskaran Toor <jaskarantoor00@gmail.com>
2024-02-05 22:04:45 -0800

    6th

--- modules/utils.js
+++ modules/utils.js
@@ -1,4 +1,7 @@
-exports.date = 
-function(){
-    return new Date();
-};
+function getDate() {
+    const currentDate = new Date();
+    return currentDate.toString();
+  }
+  
+  module.exports = { getDate };
+  
